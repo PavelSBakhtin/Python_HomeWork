ccd702e4697955b5f7f746cbe7f7f7d5ca3ccfc7
PavelSB <1987PavelS@mail.ru>
2022-12-01 02:49:16 +0300

    home work 2 task 5 done

--- homework2_task5.py
+++ homework2_task5.py
@@ -1,3 +1,19 @@
 # Реализуйте алгоритм перемешивания списка.
 
-
+import random
+l = random.randint(6, 9)
+a = []
+for i in range(l):
+    k = random.randint(97, 121)
+    r = chr(k)
+    # print(r)
+    if r in a:
+        r = chr(k + 1)
+        a.append(r)
+    else:
+        a.append(r)
+print(a)
+a[0], a[l - 3] = a[l - 3], a[0]
+a[1], a[l - 2] = a[l - 2], a[1]
+a[2], a[l - 1] = a[l - 1], a[2]
+print(a)
