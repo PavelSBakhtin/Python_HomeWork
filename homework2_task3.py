d21fe88eefd94c42965c094e56b7bf5016f62f03
PavelSB <1987PavelS@mail.ru>
2022-12-04 21:10:46 +0300

    home work 2 task 3 done

--- homework2_task3.py
+++ homework2_task3.py
@@ -8,3 +8,10 @@ c = 0
 for i in range(1, a + 1):
     c += b[i]
 print(c)
+
+# # Решение от преподавателя:
+# n = int(input())
+# summ = 0
+# for i in range(1, n + 1):
+#     summ += (1 + 1 / i) ** i
+# print(summ)
