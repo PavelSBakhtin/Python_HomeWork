0aa2f3dee8f8e81bf4d88b6e4301802f3bd2f6f1
PavelSB <1987PavelS@mail.ru>
2022-12-04 21:08:38 +0300

    home work 2 task 5 done

--- homework2_task5.py
+++ homework2_task5.py
@@ -17,3 +17,13 @@ a[0], a[l - 3] = a[l - 3], a[0]
 a[1], a[l - 2] = a[l - 2], a[1]
 a[2], a[l - 1] = a[l - 1], a[2]
 print(a)
+
+# # Решение от преподавателя:
+# import random
+# list = ["Anna", "Alex", 0, 3.14159]
+# for i in range(len(list)):
+#     index_a = random.randint(0, len(list) - 1)
+#     temp = list[i]
+#     list[i] = list[index_a]
+#     list[index_a] = temp
+# print(list)
