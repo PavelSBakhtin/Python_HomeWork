9088fb1278e10e8bd9cbccca583b0330d788d288
PavelSB <1987PavelS@mail.ru>
2022-12-12 02:04:33 +0300

    home work 5 task 2b done

---
+++ homework5_task2_b.py
@@ -0,0 +1,50 @@
+# Создайте программу для игры с конфетами человек против бота.
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход.
+
+import random
+sweets_table = int(input("Enter the sum of sweets: ")) # вводится вручную
+# sweets_table = random.randint(57, 2021) # задаётся рандомно
+# print(f"The sum of sweets on table: {sweets_table}") # количество конфет на столе
+first_player = input("Enter your name: ")
+second_player = "Bot"
+first_count = 0
+second_count = 0
+turn = random.randint(1, 2)  # жеребьёвка
+if turn == 1:
+    print(f"First turn: {first_player}")
+else:
+    print(f"First turn: {second_player}")
+
+def input_value(player, turn):
+    if turn == 1:
+        x = int(input(f"{player}, enter sun of sweets to take from 1 to 28: "))
+        while 1 > x > 28:
+            x = int(input(f"{player}, enter sun of sweets to take from 1 to 28: "))
+    else:
+        x = random.randint(1, 28)
+    return x
+
+def result(player, k, count, sweets):
+    print(f"Went down {player}, he took {k}, now he has {count}. Now on the table {sweets} sweets.")
+
+while sweets_table > 0:
+    if turn == 1:
+        k = input_value(first_player, turn)
+        first_count += k
+        sweets_table -= k
+        turn = 2
+        result(first_player, k, first_count, sweets_table)
+        continue
+    else:
+        k = input_value(second_player, turn)
+        second_count += k
+        sweets_table -= k
+        turn = 1
+        result(second_player, k, second_count, sweets_table)
+        continue
+if turn == 2:
+    print(f"The player {first_player} won")
+else:
+    print(f"The player {second_player} won")
