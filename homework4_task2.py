4f1b6a49cebd2bc5146163f4c20fecfa7de3104d
PavelSB <1987PavelS@mail.ru>
2022-12-11 23:11:40 +0300

    home work 4 task 2 done

--- homework4_task2.py
+++ homework4_task2.py
@@ -40,3 +40,17 @@ print(b)
 #     d.remove(0)
 # print(b)
 # print(d)
+
+# # Решение от преподавателя:
+# n = int(input("Введите число N: "))
+# i = 2 
+# list = []
+
+# while i <= n:
+#     if n % i == 0:
+#         list.append(i)
+#         n //= i
+#         i = 2
+#     else:
+#         i += 1
+# print(f"Простые множители введенного числа указаны в списке: {list}")
