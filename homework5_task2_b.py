e4f44b4650b800064e581ecaee98486ad012d9f8
PavelSB <1987PavelS@mail.ru>
2022-12-12 02:16:58 +0300

    home work 5 task 2b done

--- homework5_task2_b.py
+++ homework5_task2_b.py
@@ -4,7 +4,7 @@
 # Все конфеты оппонента достаются сделавшему последний ход.
 
 import random
-sweets_table = int(input("Enter the sum of sweets: ")) # вводится вручную
+sweets_table = int(input("Enter the sum of sweets: "))  # вводится вручную
 # sweets_table = random.randint(57, 2021) # задаётся рандомно
 # print(f"The sum of sweets on table: {sweets_table}") # количество конфет на столе
 first_player = input("Enter your name: ")
@@ -17,33 +17,32 @@ if turn == 1:
 else:
     print(f"First turn: {second_player}")
 
-def input_value(player, turn):
-    if turn == 1:
+def input_value(player):
+    x = int(input(f"{player}, enter sun of sweets to take from 1 to 28: "))
+    while 1 > x > 28:
         x = int(input(f"{player}, enter sun of sweets to take from 1 to 28: "))
-        while 1 > x > 28:
-            x = int(input(f"{player}, enter sun of sweets to take from 1 to 28: "))
-    else:
-        x = random.randint(1, 28)
     return x
 
 def result(player, k, count, sweets):
-    print(f"Went down {player}, he took {k}, now he has {count}. Now on the table {sweets} sweets.")
+    print(
+        f"Went down {player}, he took {k}, now he has {count}. Now on the table {sweets} sweets.")
 
 while sweets_table > 0:
     if turn == 1:
-        k = input_value(first_player, turn)
+        k = input_value(first_player)
         first_count += k
         sweets_table -= k
         turn = 2
         result(first_player, k, first_count, sweets_table)
         continue
     else:
-        k = input_value(second_player, turn)
+        k = random.randint(1, 28)
         second_count += k
         sweets_table -= k
         turn = 1
         result(second_player, k, second_count, sweets_table)
         continue
+
 if turn == 2:
     print(f"The player {first_player} won")
 else:
