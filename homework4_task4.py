8579e3bdfa5a97f547059070ceba2c258a951b82
PavelSB <1987PavelS@mail.ru>
2022-12-11 23:10:33 +0300

    home work 4 task 4 done

--- homework4_task4.py
+++ homework4_task4.py
@@ -27,3 +27,27 @@ d =  d + (f'{c[-1]} = 0')
 print(d)
 with open('homework4_task4_f1.txt', 'a') as data:
     data.write(d + '\n')
+
+# # Решение от преподавателя:
+# k = int(input('Insert equation power: '))
+# koefs = list()
+# for i in range(1, k + 2):
+#     koefs.append(randint(1, 100))
+
+# ans = list()
+# for i in range(len(koefs)):
+#     if k == 1:
+#         ans.append(f'{koefs[i]}*x')
+#     elif k == 0:
+#         ans.append(f'{koefs[i]}')
+#     else:
+#         ans.append(f'{koefs[i]}*x**{k}')
+#     flag = randint(0, 1)
+#     ans.append(str(-1**flag))
+#     k -= 1
+
+# ans.pop(-1)
+# ans.append('=0')
+# fout = open('output.txt', 'w')
+# fout.write(''.join(ans))
+# fout.close()
