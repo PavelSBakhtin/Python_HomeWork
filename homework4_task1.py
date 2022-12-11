9519da2c9006ebb1e9ad3f0d96ccae75fc99f3e4
PavelSB <1987PavelS@mail.ru>
2022-12-11 23:12:23 +0300

    home work 4 task 1 done

--- homework4_task1.py
+++ homework4_task1.py
@@ -18,3 +18,10 @@ x = str(p)
 y = x[:5]
 j = float(y)
 print(j)
+
+# # Решение от преподавателя:
+# a = int(input('введите нужную точность 1#= '))
+# pi_target = 0
+# for i in range(1, 1000000):
+#     pi_target += 4 * ((-1) ** (i + 1)) / (2 * i - 1)
+# print(str(pi_target)[:a + 2])
