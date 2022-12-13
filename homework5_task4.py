7175a2ba767bcb74435b37e0f8b2787102b7590d
PavelSB <1987PavelS@mail.ru>
2022-12-13 22:58:24 +0300

    home work 5 task 4 done

--- homework5_task4.py
+++ homework5_task4.py
@@ -1,13 +1,27 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
 text = input()
+text = text + " "
 count = 1
+a = ''
 for i in range(len(text)-1):
-    if i <= len(text):
-        if text[i] == text[i + 1]:
-            count += 1
+    if text[i] == text[i + 1]:
+        count += 1
+    else:
+        b = str(count)
+        a = a + b
+        a = a + text[i]
+        count = 1
+print(a)
+
+def decoding(text):
+    num = ''
+    res = ''
+    for i in range(len(text)):
+        if text[i].isdigit():
+            num += text[i]
         else:
-            a = text[i]
-            print(count, text[i])
-            count = 1  
-print(count, text[i])
+            res = res + text[i] * int(num)
+            num = ''
+    return res
+print(decoding(a))
