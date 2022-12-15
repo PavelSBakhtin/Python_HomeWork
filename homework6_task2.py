30c2f880cd57cb939cbf6cc71a306dc3fc183c72
PavelSB <1987PavelS@mail.ru>
2022-12-15 21:02:23 +0300

    home work 6 task 2 done

--- homework6_task2.py
+++ homework6_task2.py
@@ -8,5 +8,19 @@
 import random
 a = random.randint(5, 11)
 b = [1, 1]
-b = [b.append(b[i-2] + b[i-1]) for i in range(2, a)]
-b = [b.insert(0, b[1] - b[0]) in range(len(b)+1)]
+[b.append(b[i-2] + b[i-1]) for i in range(2, a)]
+[b.insert(0, b[1] - b[0]) for i in range(a + 1)]
+print(b)
+
+# numbers = [2, 3, 4, 5, 6, 7, 5, 4]
+# diff = [a*b for a, b in zip(numbers, numbers[:(len(numbers)//2) - 1: -1])]
+# print(diff)
+
+# t=[x*y for x, y in zip(s[-1:int(round(len(s)/2+0.1))-2:-1], s[0:int(round(len(s)/2+0.1)):1])]
+# print(t)
+
+# import random
+# a = random.randint(5, 11)
+# b = [1, 1]
+# [b.append(b[i-2] + b[i-1]) for i in range(2, a)] + [b.insert(0, b[1] - b[0]) for i in range(a + 1)]
+# print(b)
